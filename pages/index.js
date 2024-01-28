47ce045d9cc1048a3442ac6a4935cbd42dce544f
DipshikhaDas <bsse1218@iit.du.ac.bd>
2024-01-28 18:23:45 +0600

    upto 2.44 hours

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import { MongoClient } from "mongodb";
+import MeetupList from "@/components/meetups/MeetupList";
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://horrible:XVd5mCF2TBeyXDtt@cluster0.itehw1i.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString()
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps(context){
+//   const req = context.req
+//   const res = context.res
+
+//   console.log("requeset", req, "response ",res)
+//   return{
+//     props: {
+//       meetups: Dummy
+//     }
+//   }
+// }
